a52ebc10950fbce70f87d6f69961bba74a6bf05e
eemhu <125959687+eemhu@users.noreply.github.com>
2025-10-27 11:10:51 +0200

    fix license title to match GitHub description, update copyright years

--- src/test/java/com/teragrep/aer_01/fakes/RelpConnectionFake.java
+++ src/test/java/com/teragrep/aer_01/fakes/RelpConnectionFake.java
@@ -1,6 +1,6 @@
 /*
- * Teragrep Azure Eventhub Reader
- * Copyright (C) 2023  Suomen Kanuuna Oy
+ * Teragrep syslog bridge for Microsoft Azure EventHub
+ * Copyright (C) 2023-2025 Suomen Kanuuna Oy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
